2f1713e1128a9286e0797fcbfca975083fc632c8
roro-02 <85871032+roro-02@users.noreply.github.com>
2021-07-20 15:17:24 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+console.log("my first console project");
+var name = "Rohini Arya";
+console.log(name);
+var a = 10;
+var b = 5;
+var add = a + b;
+console.log(add);
+var subtract = a - b;
+console.log(subtract);
+var multiplication = a * b;
+console.log(multiplication);
+var division = a / b;
+console.log(division);
